1ded48ccb2839c77bd61812963826514685a8a4e
Code-with-satya <kumawatvishal197@gmail.com>
2023-03-23 12:47:36 +0530

    first commit

---
+++ mainjs.js
@@ -0,0 +1,35 @@
+const data = [
+    {
+        question : "2+2",
+        anwer: 4,
+        options: [5,6,4,3]
+
+    },
+    {
+        question : "20*5",
+        anwer: 100,
+        options: [80,100,90,60]
+
+    },
+    {
+        question : "20+30",
+        anwer: 50,
+        options: [50,55,66,40]
+
+    },
+    {
+        question : "2+2+2",
+        anwer: 6,
+        options: [6,8,7,4]
+
+    },
+    {
+        question : "100/5",
+        anwer: 20,
+        options: [20,30,40,50]
+
+    }
+]
+
+
+
